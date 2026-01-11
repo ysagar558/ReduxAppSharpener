e745db857c5bbb44e2ddd38c13a11861c6d9cde0
ysagar558 <ysagar558@gmail.com>
2026-01-11 13:34:45 +0530

    Incrementing and decrementing counter

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from 'redux';
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 5,
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 5,
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
